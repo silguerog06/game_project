230538a5b565638a917c7c6d582d61255c045258
SilgueroG06 <silguerog06@gmail.com>
2026-05-05 10:02:52 +0200

    [add]CombatManager,spawn_enemies()

---
+++ core/combat_manager.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var enemies_to_spawn: Array[PackedScene] = [load("res://entities/enemies/fly/enemy_fly.tscn")]
--- systems/combat/combat_scene.gd
+++ systems/combat/combat_scene.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 const PLAYER_COMBAT = preload("uid://bybydb502ovr")
 
+
+
 func _ready() -> void:
 	_spawn_player()
+	_spawn_enemies()
 
 func _process(delta: float) -> void:
 	pass
@@ -12,3 +15,26 @@ func _spawn_player() -> void:
 	var p: Node2D = PLAYER_COMBAT.instantiate()
 	p.global_position = $PlayerSpawn.global_position
 	add_child(p)
+
+func _spawn_enemies() -> void:
+	var enemies = CombatManager.enemies_to_spawn
+	var count = enemies.size()
+	var selected_markers: Array = []
+	
+	match count:
+		1:
+			selected_markers = $EnemyFormations/Formation1.get_children()
+		2:
+			selected_markers = $EnemyFormations/Formation2.get_children()
+		3:
+			selected_markers = $EnemyFormations/Formation3.get_children()
+		_:
+			selected_markers = $EnemyFormations/Formation3.get_children()
+
+	for i in range(count):
+		if i < selected_markers.size():
+			var enemy_instance = enemies[i].instantiate()
+			enemy_instance.global_position = selected_markers[i].global_position
+			add_child(enemy_instance)
+	
+	CombatManager.enemies_to_spawn.clear()
